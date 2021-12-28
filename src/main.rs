5991def560d5324efbcd22c8b06cddd7b7c90195
Filippo Berto <berto.f@protonmail.com>
2021-12-28 02:11:23 +0100

    Basic usb reporting

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,98 @@
 #![no_main]
 #![no_std]
+use keyboard_io::{
+    hid::keyboard::{KeyboardReport, USB_PRODUCT_ID, USB_VENDOR_ID},
+    HIDClass, SerializedDescriptor,
+};
 use panic_semihosting as _;
 use rtic::app;
 use stm32f4xx_hal::{
-    gpio::{gpioa::PA0, gpiod::PD12, Edge, Input, Output, PullDown, PushPull},
+    gpio::{gpioc::PC13, Output, PushPull},
+    otg_fs::{UsbBusType, USB},
     prelude::*,
+    timer,
 };
+use usb_device::{class_prelude::*, prelude::*};
 
 #[app(device = stm32f4xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
-        button: PA0<Input<PullDown>>,
-        led: PD12<Output<PushPull>>,
+        // usb_dev: UsbDevice<'static, UsbBusType>,
+        led: PC13<Output<PushPull>>,
+        usb_device: UsbDevice<'static, UsbBusType>,
+        usb_keyboard_class: HIDClass<'static, UsbBusType>,
     }
 
     #[init]
-    fn init(mut ctx: init::Context) -> init::LateResources {
-        let mut syscfg = ctx.device.SYSCFG.constrain();
+    fn init(ctx: init::Context) -> init::LateResources {
+        static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBusType>> = None;
+        static mut EP_MEMORY: [u32; 1024] = [0; 1024];
 
-        let gpiod = ctx.device.GPIOD.split();
-        let led = gpiod.pd12.into_push_pull_output();
+        let rcc = ctx.device.RCC.constrain();
+        let clocks = rcc
+            .cfgr
+            .use_hse(25.mhz())
+            .sysclk(84.mhz())
+            .require_pll48clk()
+            .freeze();
 
         let gpioa = ctx.device.GPIOA.split();
-        let mut button = gpioa.pa0.into_pull_down_input();
-        button.make_interrupt_source(&mut syscfg);
-        button.enable_interrupt(&mut ctx.device.EXTI);
-        button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Rising);
+        // let gpiob = ctx.device.GPIOB.split();
+        let gpioc = ctx.device.GPIOC.split();
+        // let gpiod = ctx.device.GPIOD.split();
 
-        init::LateResources { button, led }
+        // let led = gpiod.pd12.into_push_pull_output();
+        let mut led = gpioc.pc13.into_push_pull_output();
+        led.set_low();
+
+        let usb = USB {
+            usb_global: ctx.device.OTG_FS_GLOBAL,
+            usb_device: ctx.device.OTG_FS_DEVICE,
+            usb_pwrclk: ctx.device.OTG_FS_PWRCLK,
+            pin_dm: gpioa.pa11.into_alternate(),
+            pin_dp: gpioa.pa12.into_alternate(),
+            hclk: clocks.hclk(),
+        };
+        *USB_ALLOCATOR = Some(UsbBusType::new(usb, EP_MEMORY));
+        let usb_allocator = USB_ALLOCATOR.as_ref().unwrap();
+
+        let usb_keyboard_class = HIDClass::new(usb_allocator, KeyboardReport::desc(), 10);
+        let usb_device =
+            UsbDeviceBuilder::new(usb_allocator, UsbVidPid(USB_VENDOR_ID, USB_PRODUCT_ID))
+                .manufacturer("Bertof")
+                .product("lets split")
+                .serial_number(env!("CARGO_PKG_VERSION"))
+                .build();
+
+        let mut timer = timer::Timer::new(ctx.device.TIM3, &clocks).start_count_down(1.khz());
+        timer.listen(timer::Event::TimeOut);
+
+        init::LateResources {
+            led,
+            usb_device,
+            usb_keyboard_class,
+        }
     }
 
-    #[task(binds = EXTI0, resources = [button, led])]
-    fn button_click(ctx: button_click::Context) {
-        ctx.resources.button.clear_interrupt_pending_bit();
-        ctx.resources.led.toggle();
+    #[task(binds = OTG_FS, priority = 2, resources = [usb_device, usb_keyboard_class])]
+    fn usb_tx(ctx: usb_tx::Context) {
+        if ctx
+            .resources
+            .usb_device
+            .poll(&mut [ctx.resources.usb_keyboard_class])
+        {
+            ctx.resources.usb_keyboard_class.poll();
+        }
+    }
+
+    #[task(binds = OTG_FS_WKUP, priority = 2, resources = [usb_device, usb_keyboard_class])]
+    fn usb_rx(ctx: usb_rx::Context) {
+        if ctx
+            .resources
+            .usb_device
+            .poll(&mut [ctx.resources.usb_keyboard_class])
+        {
+            ctx.resources.usb_keyboard_class.poll();
+        }
     }
 };
