50c83d75702e56e5555d82a602b9b136ab2b2762
Filippo Berto <berto.f@protonmail.com>
2022-02-06 23:47:41 +0100

    Fix logging + local grid events example

---
+++ src/bin/t_local_grid.rs
@@ -0,0 +1,83 @@
+#![no_main]
+#![no_std]
+
+use lets_split as _; // global logger + panicking-behavior + memory layout
+
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
+mod app {
+    use defmt::info;
+    use keyboard_io::{buttons::LocalGrid, prelude::PinState};
+    use stm32f4xx_hal::{
+        gpio::{EPin, Input, Output, PullUp, PushPull},
+        pac,
+        prelude::*,
+        timer,
+    };
+
+    // Shared resources go here
+    #[shared]
+    struct Shared {}
+
+    // Local resources go here
+    #[local]
+    struct Local {
+        local_grid: LocalGrid<EPin<Input<PullUp>>, EPin<Output<PushPull>>, 2, 3>,
+        timer: timer::CountDownTimer<pac::TIM3>,
+    }
+
+    #[init()]
+    fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+        let rcc = c.device.RCC.constrain();
+        let clocks = rcc
+            .cfgr
+            .use_hse(25.mhz())
+            .sysclk(84.mhz())
+            .require_pll48clk()
+            .freeze();
+
+        let mut timer = timer::Timer::new(c.device.TIM3, &clocks).start_count_down(1.khz());
+        timer.listen(timer::Event::TimeOut);
+
+        let gpioa = c.device.GPIOA.split();
+        let gpiob = c.device.GPIOB.split();
+        let gpioc = c.device.GPIOC.split();
+
+        let mut led = gpioc.pc13.into_push_pull_output();
+        led.set_low();
+
+        let inputs = [
+            gpiob.pb1.into_pull_up_input().erase(),
+            gpiob.pb0.into_pull_up_input().erase(),
+        ];
+        let outputs = [
+            gpioa.pa6.into_push_pull_output().erase(),
+            gpioa.pa7.into_push_pull_output().erase(),
+            gpioa.pa5.into_push_pull_output().erase(),
+        ];
+
+        let local_grid = LocalGrid::new(inputs, outputs, PinState::Low);
+
+        (Shared {}, Local { local_grid, timer }, init::Monotonics())
+    }
+
+    // Optional idle, can be removed if not needed.
+    // Note that removing this will put the MCU to sleep when no task is running, and this
+    // generally breaks RTT based printing.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        defmt::info!("idle");
+
+        loop {
+            continue;
+        }
+    }
+
+    #[task(binds = TIM3, priority = 1, local = [timer, local_grid])]
+    fn tick(c: tick::Context) {
+        c.local.timer.wait().ok();
+
+        for e in c.local.local_grid.get_events() {
+            info!("Event: {:?}", e);
+        }
+    }
+}
